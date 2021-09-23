383a83168e31405752913c4f762e724955bfaf15
Carly Silva <carly.fs.silva@gmail.com>
2021-09-23 11:58:38 -0300

    testing import

--- orm/typeorm/users/index.ts
+++ orm/typeorm/users/index.ts
@@ -1,4 +1,4 @@
-export * as Address from './Address';
-export * as Role from './Role';
-export * as User from './User';
-export * as Phone from './Phone'
+export * from './Address';
+export * from './Role';
+export * from './User';
+export * from './Phone'
